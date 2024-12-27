2e988e73895936058583ac1307d7fb0b879472b5
Steven Leal <mitsurukudo213@gmail.com>
2024-12-27 05:04:42 -0600

    [fix] fixed authmiddleware error

--- server/src/middleware/AuthMiddleware.ts
+++ server/src/middleware/AuthMiddleware.ts
@@ -4,33 +4,31 @@ import { AsyncWrapper, ErrorMessage, HttpStatusCode } from "../utils";
 import { UserModelType } from '../types.def';
 import { UserModel } from '../models';
 
-
 export const AuthMiddleware = AsyncWrapper(async(req: Request, res: Response, next:NextFunction) => {
     const token = req.cookies['foodZone']
 
-
     if(!token) {
-        return res.status(HttpStatusCode.UNAUTHORIZED).json({message: ErrorMessage.NOT_AUTHORIZED})
+        return res.status(HttpStatusCode.UNAUTHORIZED).json({message: ErrorMessage.NOT_AUTHORIZED});
     }
 
-    const decodeToken = await jwt.verify(token, process.env.SECRET_KEY)
+    const decodeToken = await jwt.verify(token, process.env.SECRET_KEY);
 
     if(!decodeToken) {
-        return res.status(HttpStatusCode.UNAUTHORIZED).json({message: ErrorMessage.NOT_AUTHORIZED})
+        return res.status(HttpStatusCode.UNAUTHORIZED).json({message: ErrorMessage.NOT_AUTHORIZED});
     }
 
-    req.userId = (decodeToken as JwtPayload).userId
-    next()
+    req.userId = (decodeToken as JwtPayload).userId;
+    next();
 })
 
 export const AdminMiddleware = async(req:Request, res:Response, next:NextFunction) => {
     try {
-        const findAdmin: UserModelType | null = await UserModel.findById(req.userId)
+        const findAdmin: UserModelType | null = await UserModel.findById(req.userId);
         if(findAdmin?.role !== 'admin') {
-            return res.status(401).json({message: "Unauthorized"})
+            return res.status(401).json({message: "Unauthorized"});
         }
         next()
     } catch (error) {
-        return res.status(401).json({message: 'Unauthorized'})
+        return res.status(401).json({message: 'Unauthorized'});
     }
 }
